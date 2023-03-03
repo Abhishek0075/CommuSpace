a765e680b67161c29571b99648b8458dd65facdb
abhirajp075@gmail.com <abhirajp075@gmail.com>
2023-03-03 09:23:21 +0530

    new changes and db connection done

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 const express = require("express")
 const chats = require("./data/data.js")
 const dotenv = require("dotenv")
+const connectDB = require("./config/db.js")
 
 const app = express()
 dotenv.config() // helps to read the env file
-
+connectDB()
 
 app.get("/",function(req,res){
     res.send("API is running")
 })
 
-app.get("/api/chats",function(req,res){
+app.get("/chats",function(req,res){
     res.send(chats)
 })
 
-app.get("/api/chats/:id",function(req,res){
+app.get("/chats/:id",function(req,res){
     // console.log(req.params.id);
     const singleChat = chats.find((c)=>c._id === req.params.id) // find gives the elements of a list
     res.send(singleChat)                                        // satisfying the given condition
@@ -22,4 +23,4 @@ app.get("/api/chats/:id",function(req,res){
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT,console.log("Server Started on PORT ${PORT}"));
+app.listen(PORT,console.log("Server Started on PORT ",PORT));
