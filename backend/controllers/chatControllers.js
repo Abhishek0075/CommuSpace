2613e69b6242f33712b758e4ccc21ecac0da0f5c
abhirajp075@gmail.com <abhirajp075@gmail.com>
2023-03-26 15:10:31 +0530

    create groupChat API created

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,5 +1,4 @@
 const asyncHandler = require("express-async-handler")
-const UserCommunity = require("../models/usersInCommunity")
 const Community = require("../models/communityModel")
 const Users = require("../models/usersModel")
 
